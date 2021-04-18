2d196c4ff930aaec170b3801183d36a9909927fa
Pranav Baburaj <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-18 23:01:45 +0530

    Update main.d

--- d/main.d
+++ d/main.d
@@ -5,12 +5,26 @@ import core.stdc.stdlib : exit;
 import std.string;
 import std.file;
 
+/*
+The loo exception class to
+create exception messafes 
+when an error occurs
+*/
 class LooException {
 	private string message;
 	private string suggestion;
 	private string file;
 	private string line;
 
+	/*
+	The constructor for the looexception
+	class
+
+	message(str) - The error message
+	suggestion(str) - Try again message
+	file(str) - The file in which the error occured
+	line(int) - The line number at which the error occured
+	*/
 	this(string message, string suggestion, string file, int line){
 		this.message = message;
 		this.suggestion = suggestion;
@@ -29,33 +43,65 @@ class LooException {
 	}
 }
 
+/*
+The main language class where
+the code is executed
+*/
 class LooLang {
 	private immutable string data;
 	private int line_number;
 	private int position;
 	private int ascii;
 
+	/**
+	The constructor for the LooLang 
+
+	data(readonly str) - The code
+	line_number(int) - The current line number
+	position(int) - the parser position
+	*/
 	this(string data){
 		this.data = data;
 		this.line_number = 1;
 		this.position = 0;
 	}
 
+	/**
+	Execute the script
+	*/
 	public void executeLooLanguage() {
 		char character = this.current_character();
+		// stop the code if the executor
+		// finds a semicolon(;).
 		while(character != ';'){
+			// increment the ascii value
+			// if the character is +
 			if(character == '+'){
 				this.ascii += 1;
 			} else if(character == '-'){
+				// decrement the ascii value
+				// if the character is -
 				this.ascii -= 1;
 			} else if(character == '#'){
+				// print the ascii value in character
+				// form if the character is #
 				write(cast(char) this.ascii);
+			} else if(character == '\n'){
+				// increment the line number
+				// if the executor finds a newline
+				// character
+				this.line_number += 1;
+			} else if(character == ';') {
+				break;
 			}
 			this.position += 1;
 			character = this.current_character();
 		}
 	}
 
+	/**
+	Get the current character
+	*/
 	private char current_character() {
 		if(this.data.length == this.position){
 			return ';';
