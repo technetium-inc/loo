bd76ad7702983476b78ce9b08eac5bc1b4d68984
Pranav Baburaj <70764593+pranavbaburaj@users.noreply.github.com>
2021-09-19 21:39:18 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,67 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
-class LooException {
-    public:
-    std::string message;
-    int line_number;
+class Interpreter {
+public:
+  std::string source;
 
-    LooException(std::string message, int line) {
-        this->message = message;
-        this->line_number = line;
+  /**
+  Get the name of the file from the command line
+  arguments passed in. returns null if a filename is
+  not given;
+  */
+  static std::string getFileName(const char *argv[], int length) {
+    if (length == 1)
+      return nullptr;
+    return argv[1];
+  }
+
+  /**
+  Returns the content of the file.
+  */
+  static std::string getFileContent(const std::string filename) {
+    std::ifstream file(filename);
+    std::string fileContent = "";
+    std::string currentLine;
+    while (getline(file, currentLine)) {
+      fileContent += currentLine;
     }
+    file.close();
+    return fileContent;
+  }
 
-    int throw_exception(int exit_status, bool fatal){
-        std::cout << "[Error] " << this->message << " at line " <<this->line_number;
-        if(fatal){
-            std::exit(exit_status);
-        }
-        return exit_status;
+  Interpreter(const std::string source) {
+    this->source = source;
+    interpret();
+  }
+
+private:
+  void interpret() {
+    int asciiCharacter = 0;
+    for (int index = 0; index < source.length(); index++) {
+      const char currentCharacter = source[index];
+      switch (currentCharacter) {
+      case '+':
+        asciiCharacter += 1;
+        break;
+      case '-':
+        asciiCharacter -= 1;
+        break;
+      case ';':
+        exit(1);
+        break;
+      case '#':
+        printf("%c", (char)asciiCharacter);
+        break;
+      }
     }
+  }
 };
 
+int main(int argc, const char *argv[]) {
+  const std::string filename = Interpreter::getFileName(argv, argc);
+  const std::string fileContent = Interpreter::getFileContent(filename);
 
-class FileReader {
-    public:
-    std::string filename;
-
-    FileReader(std::string filename){
-        this->filename = filename;
-    }
-
-    std::string read() {
-        std::string line;
-        std::ifstream file(this->filename);
-        if(file.is_open()){
-            std::string file_content;
-            while(getline(file, line)){
-                file_content += line;
-            }
-            return file_content;
-        } 
-
-        LooException *exception = new LooException("Unable to read file", -1);
-        exception->throw_exception(0, true);
-    }
-};
-
-class LooLang {
-    public:
-    std::string source;
-    int position;
-    int memory_cell;
-
-    LooLang(std::string source_code){
-        this->source = source_code;
-        this->position = 0;
-        this->memory_cell = 0;
-    }
-
-    void execute() {
-        char character = this->current_character();
-        while(character != ';'){
-            if(character == '+'){
-                this->memory_cell += 1;
-            } else if(character == '-'){
-                this->memory_cell -= 1;
-            } else if(character == '#'){
-                char output = this->memory_cell;
-                std::cout << output;
-            }
-            this->position += 1;
-            character = this->current_character();
-        }
-    }
-
-    private:
-    char current_character() {
-        if(this->source.length() == this->position){
-            return ';';
-        } else {            
-            return this->source[this->position];
-        }
-    }
-};
-
-std::string filename(int argc, const char *argv[]) {
-    if(argc == 1){
-        LooException *exception = new LooException("Expected a filename", -1);
-        exception->throw_exception(0, true);
-    } else {
-        FileReader *reader = new FileReader(argv[1]);
-        return reader->read();
-    }
-}
-
-int main(int argc, char const *argv[]){
-    std::string file = filename(argc, argv);
-    LooLang *loo = new LooLang(file);
-    loo->execute();
-    return 0;
+  const Interpreter interpreter = *new Interpreter(fileContent);
+  return 0;
 }
